515be3162c98746df5c1bb26f1aa396b8345337f
True <jjst1746@gmail.com>
2022-04-08 18:01:01 +0200

    Create memory.py

---
+++ memory.py
@@ -0,0 +1,20 @@
+import random
+
+def create_memory():
+    while True:
+        """set1 = []
+        set2 = []
+
+        name = input('Please create your first object.')
+        set1.append(name)
+        name = input('Please create your first object.')
+        set2.append(name)
+        memory = set1 + set2
+        print(memory)"""
+
+        num = int(input())
+        sum = sum + num
+        print(sum)
+
+
+create_memory()
